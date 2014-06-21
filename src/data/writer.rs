0a7e304645b3a9d728f68934167706f11a1468f2
Daniel Fagnan <dnfagnan@gmail.com>
2014-06-21 13:46:36 -0600

    Cleaned up

--- src/data/writer.rs
+++ src/data/writer.rs
@@ -2,36 +2,55 @@ use result::DataResult;
 use std::io::MemWriter;
 use serialize;
 use serialize::Encodable;
-use std::io::{IoError, IoResult};
+use std::io::{IoError, IoResult, Writer};
 
+use encode;
 use bytecode;
 
-
-/// A writer is responsible for encoding the data. The output is simply a stream of
-/// u8s (a stream of bytes).
-///
-/// If a chunked encoding is used, the stream is not closed immediately. More items can be pushed until
-/// the writer is finished.
-pub trait Writer {
-    fn write<T: Encodable<Encoder, IoError>>(&mut self, value: T) -> DataResult<()>;
-}
-
 /// DataWriter is the default implementation for the writer.
-pub struct Encoder {
-    stream: MemWriter
+pub struct Encoder<'a> {
+    wr: &'a mut Writer
 }
 
-impl serialize::Encoder<IoError> for Encoder {
+impl<'a> Encoder<'a> {
+
+    /// Create a new data writer with an open stream to begin with.
+    pub fn new<'a>(wr: &'a mut Writer) -> Encoder<'a> {
+        Encoder {
+            wr: wr
+        }
+    }
+
+}
+
+impl<'a> serialize::Encoder<IoError> for Encoder<'a> {
     fn emit_nil(&mut self) -> IoResult<()> { Ok(()) }
 
-    fn emit_uint(&mut self, v: uint) -> IoResult<()> { Ok(()) }
-    fn emit_u64(&mut self, v: u64) -> IoResult<()> { Ok(()) }
+    fn emit_uint(&mut self, v: uint) -> IoResult<()> {
+        Ok(())
+    }
+
+    fn emit_u64(&mut self, v: u64) -> IoResult<()> {
+        Ok(())
+    }
+
     fn emit_u32(&mut self, v: u32) -> IoResult<()> { Ok(()) }
     fn emit_u16(&mut self, v: u16) -> IoResult<()> { Ok(()) }
     fn emit_u8(&mut self, v: u8) -> IoResult<()> { Ok(()) }
 
     fn emit_int(&mut self, v: int) -> IoResult<()> { Ok(()) }
-    fn emit_i64(&mut self, v: i64) -> IoResult<()> { Ok(()) }
+    fn emit_i64(&mut self, v: i64) -> IoResult<()> {
+        let u = v as u64;
+        write!(self.wr, "{}", (u >> 56) as u8);
+        write!(self.wr, "{}", (u >> 48) as u8);
+        write!(self.wr, "{}", (u >> 40) as u8);
+        write!(self.wr, "{}", (u >> 32) as u8);
+        write!(self.wr, "{}", (u >> 24) as u8);
+        write!(self.wr, "{}", (u >> 16) as u8);
+        write!(self.wr, "{}", (u >> 8) as u8);
+        write!(self.wr, "{}", (u >> 0) as u8);
+        Ok(())
+    }
     fn emit_i32(&mut self, v: i32) -> IoResult<()> { Ok(()) }
     fn emit_i16(&mut self, v: i16) -> IoResult<()> { Ok(()) }
     fn emit_i8(&mut self, v: i8) -> IoResult<()> { Ok(()) }
@@ -46,20 +65,20 @@ impl serialize::Encoder<IoError> for Encoder {
 
     fn emit_enum(&mut self,
                  name: &str,
-                 f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                 f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
     fn emit_enum_variant(&mut self,
                         _: &str,
                         _: uint,
                         len: uint,
-                        f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                        f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
     fn emit_enum_variant_arg(&mut self,
                              id: uint,
-                             f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                             f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
@@ -67,58 +86,58 @@ impl serialize::Encoder<IoError> for Encoder {
                                 _: &str,
                                 _: uint,
                                 len: uint,
-                                f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                                f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
     fn emit_enum_struct_variant_field(&mut self,
                                       _: &str,
                                       _: uint,
-                                      f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                                      f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
     fn emit_struct(&mut self,
                    name: &str,
                    len: uint,
-                   f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                   f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
     fn emit_struct_field(&mut self,
                          name: &str,
                          id: uint,
-                         f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                         f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
     fn emit_tuple(&mut self,
                   id: uint,
-                  f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                  f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
     fn emit_tuple_arg(&mut self,
                       len: uint,
-                      f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                      f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
     fn emit_tuple_struct(&mut self,
                          name: &str,
                          len: uint,
-                         f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                         f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
 
     fn emit_tuple_struct_arg(&mut self,
                              len: uint,
-                             f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                             f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
-    fn emit_option(&mut self, f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+    fn emit_option(&mut self, f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
@@ -126,62 +145,57 @@ impl serialize::Encoder<IoError> for Encoder {
         Ok(())
     }
 
-    fn emit_option_some(&mut self, f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+    fn emit_option_some(&mut self, f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
     fn emit_seq(&mut self,
                 len: uint,
-                f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
     fn emit_seq_elt(&mut self,
                     id: uint,
-                    f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                    f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
     fn emit_map(&mut self,
                 len: uint,
-                f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
     fn emit_map_elt_key(&mut self,
                         id: uint,
-                        f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                        f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 
     fn emit_map_elt_val(&mut self,
                         id: uint,
-                        f: |&mut Encoder| -> IoResult<()>) -> IoResult<()> {
+                        f: |&mut Encoder<'a>| -> IoResult<()>) -> IoResult<()> {
         Ok(())
     }
 }
 
-pub struct DataWriter {
-    encoder: Encoder
-}
-
-impl Encoder {
-
-    /// Create a new data writer with an open stream to begin with.
-    pub fn new() -> Encoder {
-        Encoder {
-            stream: MemWriter::new()
-        }
-    }
-}
-
-impl Writer for DataWriter {
-    fn write<T: Encodable<Encoder, IoError>>(&mut self, value: T) -> DataResult<()> {
-        Ok(())
-    }
-}
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use serialize::Encodable;
+    use std::io::MemWriter;
+
+    #[test]
+    fn sint() {
+        let mut writer = MemWriter::new();
+        {
+            let mut encoder = Encoder::new(&mut writer);
+            let _ = 5i64.encode(&mut encoder);
+        }
+
+        fail!("{}", writer.unwrap());
+    }
+
 }
